2b5d5ae85e63b301a26f1d29014efb1c75a4ac1a
Benjamin Sommerfeld <bsommerfeld@users.noreply.github.com>
2025-06-18 01:16:52 +0200

    fix: simplify G-cost calculation by removing redundant movement cost logic in Node

--- engine/src/main/java/de/bsommerfeld/pathetic/engine/Node.java
+++ engine/src/main/java/de/bsommerfeld/pathetic/engine/Node.java
@@ -137,18 +137,7 @@ public class Node implements Comparable<Node> {
    */
   public double getGCost() {
     if (this.parent == null) return 0.0;
-
-    return this.gCost > 0
-        ? this.gCost
-        : (parent.getGCost() + calculateMovementCost(parent.getPosition(), this.position));
-  }
-
-  private double calculateMovementCost(PathPosition from, PathPosition to) {
-    if (from.getFlooredX() != to.getFlooredX() && from.getFlooredZ() != to.getFlooredZ()) {
-      return Math.sqrt(2); // Diagonal
-    } else {
-      return 1.0; // Horizontal/Vertical
-    }
+    return this.gCost;
   }
 
   @Override
